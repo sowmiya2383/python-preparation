ff13c809b5cebfbf832576530b1bbedf18486844
Sowmiya Arun <sowmya.arun2383@gmail.com>
2026-03-31 23:15:12 +0530

    Add files via upload

--- code_practice/5.count_freq.py
+++ code_practice/5.count_freq.py
@@ -1,1 +1,9 @@
-ar=list(map(int.input()))
+arr = list(map(int,input().split()))
+freq = {}
+for i in arr:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+for i, count in freq.items():
+    print(i, count)
